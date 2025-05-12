a9b3f9acf814260f1724c946e8e790406bc9accd
Bogdan Necatu <necatubogdan@gmail.com>
2025-05-12 18:20:51 +0200

    feat: new color for header and footer in dark mode

--- src/shared/ui/Footer/Footer.tsx
+++ src/shared/ui/Footer/Footer.tsx
@@ -2,7 +2,7 @@
 
 export default function Footer() {
   return (
-    <footer className="bg-zinc-900 text-white text-xs text-center py-6 border-t border-white/10 mt-auto">
+    <footer className="bg-zinc-900 text-white text-xs text-center py-6 border-t border-white/10 dark:bg-yellow-950 dark:text-yellow-100 transition-colors">
       <span suppressHydrationWarning>
         DashPilot © 2025 — All rights reserved
       </span>
